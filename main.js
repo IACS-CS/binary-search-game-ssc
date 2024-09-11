01016f14211603caab0c19968b9e0d64b016053d
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-11 10:33:36 -0400

    Update main.js
    Revert accidental commits of student code back to here ???

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 import "./style.css";
 import { TextInterface } from "text-interface";
 
-
-let binary = 0;
-
-
-// Get the element with id="app"
 let app = document.querySelector("#app");
-
 // Create a new "Text Interface"
-
-let ti = new TextInterface(app, "What chocolate bar are you?");
+let ti = new TextInterface(app, "Example Text Interface");
 ti.output("What is your name?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
-
-
