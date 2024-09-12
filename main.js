c97130469d481024cd208bf9da878f4cc0972679
cee1904 <itscece890@gmail.com>
2024-09-12 16:43:13 +0000

    slytherin option edits cik, sb, sk

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ let ti = new TextInterface(app, "Guess Your Hogwarts House");
 ti.output("Are you brave?");
 let brave = await ti.readYesOrNo();
 if (brave) {
-  ti.output("You are a Gryffindor!")
+  ti.output("You are a Gryffindor!");
 } else {
-}
-
-let compassionate = await ti.readYesOrNti.output("Are you compassionate?");o();
-if (compassionate) {
-  ti.output("You are a Hufflepuff!")
-} else 
-
-let ambitious = await ti.readYesOrNo();
-if (ambitious) {
-  ti.output("You are a Slytherin!")
-}
+  ti.output("Are you compassionate?");
+  let compassionate = await ti.readYesOrNo();
+    if (compassionate) {
+    ti.output("You are a Hufflepuff!");
+} else {
+   ti.output("Are you ambitious?");
+  let ambitious = await ti.readYesOrNo();
+  if (ambitious) {
+    ti.output("You are a Slytherin!");
+  } else {
+  }
+}}
