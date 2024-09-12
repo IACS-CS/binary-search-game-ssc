fe7c46b55a30ae95aada1a22a7d5ec8aa3cf7f72
cee1904 <itscece890@gmail.com>
2024-09-12 16:40:07 +0000

    slytherin option cik, sk, sb

--- main.js
+++ main.js
@@ -8,4 +8,15 @@ ti.output("Are you brave?");
 let brave = await ti.readYesOrNo();
 if (brave) {
   ti.output("You are a Gryffindor!")
+} else {
+}
+
+let compassionate = await ti.readYesOrNti.output("Are you compassionate?");o();
+if (compassionate) {
+  ti.output("You are a Hufflepuff!")
+} else 
+
+let ambitious = await ti.readYesOrNo();
+if (ambitious) {
+  ti.output("You are a Slytherin!")
 }
