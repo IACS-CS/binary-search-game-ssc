16e87913ef3908f162bcf8db594df3310421223b
cee1904 <itscece890@gmail.com>
2024-09-17 14:46:11 +0000

    blue option- cik sb sk

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Guess The Color of the Rainbow!");
-ti.output("Is it a primary color?");
+ti.output("Is it a primary color? (red, blue, yellow)");
 let primary = await ti.readYesOrNo();
 if (primary) {
   ti.output("Is it a warm color?");
@@ -14,9 +14,20 @@ if (primary) {
     let red = await ti.readYesOrNo();
     if (red) { 
       ti.output("Ooh! Nice color!")
+    } else { 
+      let yellow = await ti.readYesOrNo ();
+      if (yellow) {
+      ti.output("Ooh! Nice color!")
+      }}
+  } else ( 
+    ti.output("Your color is blue! I like that color too!")
+
+    // cool
+
+  )
     }
-}} else {
-  ti.output("Is it a secondary color?");
+ else {
+  ti.output("Is it a secondary color? (orange, green, purple)");
   let secondary = await ti.readYesOrNo();
     if (secondary) {
     ti.output("Is it a cool color?");
